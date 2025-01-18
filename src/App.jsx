571b08ab3c983c9a3c9a23d1232522889479f8f6
Robert2547 <91036825+Robert2547@users.noreply.github.com>
2025-01-17 21:04:16 -0500

    Github Deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // src/App.jsx
 import React from "react";
+import { HashRouter as Router } from "react-router-dom";
 import Navigation from "./components/layout/Navigation";
 import Hero from "./components/sections/Hero";
 import Skills from "./components/sections/Skills";
@@ -14,46 +15,48 @@ import "./styles/animations.css";
 
 function App() {
   return (
-    <div className="relative">
-      {/* Background Effects */}
-      <div className="fixed inset-0">
-        <SpaceScene />
-        <FloatingAsteroids />
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-background/50 to-background pointer-events-none" />
+    <Router>
+      <div className="relative">
+        {/* Background Effects */}
+        <div className="fixed inset-0">
+          <SpaceScene />
+          <FloatingAsteroids />
+          <div className="absolute inset-0 bg-gradient-to-b from-transparent via-background/50 to-background pointer-events-none" />
+        </div>
+
+        {/* Content */}
+        <div className="relative z-10">
+          <Navigation />
+          <main className="space-y-0">
+            <div className="relative">
+              <Hero />
+              <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
+            </div>
+
+            <div className="relative bg-nebula-1">
+              <Skills />
+              <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
+            </div>
+
+            <div className="relative bg-nebula-2">
+              <Experience />
+              <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
+            </div>
+
+            <div className="relative">
+              <Projects />
+              <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
+            </div>
+
+            <div className="relative bg-nebula-1">
+              <Contact />
+              <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
+            </div>
+          </main>
+          <Footer />
+        </div>
       </div>
-
-      {/* Content */}
-      <div className="relative z-10">
-        <Navigation />
-        <main className="space-y-0">
-          <div className="relative">
-            <Hero />
-            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
-          </div>
-
-          <div className="relative bg-nebula-1">
-            <Skills />
-            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
-          </div>
-
-          <div className="relative bg-nebula-2">
-            <Experience />
-            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
-          </div>
-
-          <div className="relative">
-            <Projects />
-            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
-          </div>
-
-          <div className="relative bg-nebula-1">
-            <Contact />
-            <div className="absolute bottom-0 left-0 w-full h-24 bg-gradient-to-t from-background to-transparent" />
-          </div>
-        </main>
-        <Footer />
-      </div>
-    </div>
+    </Router>
   );
 }
 
